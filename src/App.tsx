277a0dbe0c1c5dcaf1b9d266f1091045d9d30db2
Julie Muzina <jmuzina@jmuzina.io>
2024-09-11 16:56:33 -0400

    boilerplate

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.scss';
+
+function App() {
+  return (
+      <div id={"app"}>
+        Welcome to my cool app
+      </div>
+  );
+}
+
+export default App;
